2e74d778b0f2c22b2469ba18c415811fdcd775d1
Jklimxx <90564234+Jklimxx@users.noreply.github.com>
2021-12-13 23:00:40 +0700

    Add files via upload

---
+++ ex4/js_ex4_2.js
@@ -0,0 +1,5 @@
+//JavaScript by Pornkamol Pienpakdee 1630902904 
+//Ex4.2: JavaScript Numbers and Strings
+document.getElementById("ex4-2-1").innerHTML = 93.69; //Number
+document.getElementById("ex4-2-2").innerHTML = "Aj.NesT"; //String
+document.getElementById("ex4-2-3").innerHTML = "Aj.NesT score: " + 93.69; //Number + String
